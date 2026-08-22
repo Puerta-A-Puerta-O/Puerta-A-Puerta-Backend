dc8136b0e88ce6367dcc4242f5dd59b1c0e71b1f
jonjonathanarias <106196489+jonjonathanarias@users.noreply.github.com>
2026-08-22 00:57:43 -0300

    Agregamos a la capa de Seguridad y Rate Limiting

--- src/app.js
+++ src/app.js
@@ -1,38 +1,60 @@
 // src/app.js
 const express = require('express');
 const cors = require('cors');
-require('dotenv').config();
-
-const authRoutes = require('./routes/authRoutes');
-const orderRoutes = require('./routes/orderRoutes');
-const errorHandler = require('./middlewares/errorHandler');
-const productRoutes = require('./routes/productRoutes');
-
-const app = express();
-
+const helmet = require('helmet');
+const rateLimit = require('express-rate-limit');
 const swaggerUi = require('swagger-ui-express');
 const swaggerSpec = require('./config/swagger');
 
+// Rutas
+const authRoutes = require('./routes/authRoutes');
+const orderRoutes = require('./routes/orderRoutes');
+const productRoutes = require('./routes/productRoutes'); // Importamos productRoutes
+
+const app = express();
+
+// 1. Seguridad de Cabeceras HTTP
+app.use(helmet());
+
+// Desactivar limitadores durante la ejecución de tests
+const isTestEnv = process.env.NODE_ENV === 'test';
+
+// 2. Limitadores de Rate Limit
+const limiterGlobal = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+  standardHeaders: true,
+  legacyHeaders: false,
+  skip: () => isTestEnv,
+  message: {
+    status: 'fail',
+    message: 'Demasiadas peticiones desde esta IP, por favor intente más tarde.'
+  }
+});
+app.use('/api/', limiterGlobal);
+
+const authLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 5,
+  standardHeaders: true,
+  legacyHeaders: false,
+  skip: () => isTestEnv,
+  message: {
+    status: 'fail',
+    message: 'Demasiados intentos de inicio de sesión. Cuenta bloqueada temporalmente por 15 minutos.'
+  }
+});
+
+// Middlewares Base
 app.use(cors());
 app.use(express.json());
 
-
-// Rutas
-app.use('/api/v1/auth', authRoutes);
-app.use('/api/v1/pedidos', orderRoutes);
-app.use('/api/v1/locales/:localId/productos', productRoutes);
+// Documentación Swagger
 app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
-
-// Manejo de 404
-app.use((req, res, next) => {
-  res.status(404).json({
-    status: 'fail',
-    mensaje: `No se encontró la ruta ${req.originalUrl} en este servidor`,
-  });
-});
-
-// Middleware de errores
-app.use(errorHandler);
+// Rutas montadas
+app.use('/api/v1/auth', authLimiter, authRoutes);
+app.use('/api/v1/pedidos', orderRoutes);
+app.use('/api/v1/locales/:localId/productos', productRoutes); // Mapeo anidado directo
 
 module.exports = app;
